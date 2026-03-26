e3689dc0fe917e56b2ac7bf36bb40d97d43a125a
Igihozo Belise <igihozobelise6@gmail.com>
2026-03-26 11:30:13 +0200

    added a  new lesson

--- swapping dsa.cpp
+++ swapping dsa.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 using namespace std;
+
+// Function to swap two numbers using pass by reference
 void swap(int &x, int &y){
-	int temp = x;
-	x = y;
-	y = temp; 
+    // Create a temporary variable to store the value of x
+    int temp = x;
 
+    // Assign the value of y to x
+    x = y;
+
+    // Assign the stored value (original x) to y
+    y = temp; 
 }
-int main(){
-	
-	int x = 2, y = 3;
-	
-	cout << "Before swapping: x = " << x << " and y = " << y << endl;
-	swap(x,y);
-	cout << "After swapping: x = " << x << " and y = " << y ;
-	return 0;
-	
 
-	 
-	
+int main(){
+    
+    // Declare and initialize two variables
+    int x = 2, y = 3;
+    
+    // Display values before swapping
+    cout << "Before swapping: x = " << x << " and y = " << y << endl;
+
+    // Call swap function (values will be swapped because of reference)
+    swap(x, y);
+
+    // Display values after swapping
+    cout << "After swapping: x = " << x << " and y = " << y;
+
+    return 0;
 }
