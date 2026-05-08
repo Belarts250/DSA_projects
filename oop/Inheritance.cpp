d4cf8cb1d18b900ada5c893125c81cf312745c97
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-08 18:18:58 +0200

    explanations

--- oop/Inheritance.cpp
+++ oop/Inheritance.cpp
@@ -13,7 +13,7 @@ class Car{
 	}
 };
 
-// RwandaCar is a child class of Car, it inherits all the properties and methods of Car
+// RwandaCar is a child class of Car, it inherits all the properties and methods of 
 class RwandaCar : public Car {
 	public:
 	string country;
