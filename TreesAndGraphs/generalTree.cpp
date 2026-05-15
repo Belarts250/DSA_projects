9881af392fe25ddf461320fb2b70947bb2af431d
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-15 20:48:20 +0200

    tree lesson

---
+++ TreesAndGraphs/generalTree.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 100;
+vector<int> tree[N];
+
+void bfs(int root){
+    queue<int> q;
+    q.push(root);
+    cout << "BFS (Level Order Traversal): ";
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+
+        cout << node << " ";
+
+
+        for (int child : tree[node]) {
+            q.push(child);
+        }
+    }
+    cout << endl;
+
+}
