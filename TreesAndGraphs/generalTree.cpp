58bc419ff0ed665f9d251bbd2236739029b3d17a
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-19 11:28:11 +0200

    new

--- TreesAndGraphs/generalTree.cpp
+++ TreesAndGraphs/generalTree.cpp
@@ -35,7 +35,7 @@ int main(){
     for(int i = 0; i < edges; i++){
         int u, v;
 
-        cin >> u >> v; // parent-child
+        cin >> u >> v; // parent
         tree[u].push_back(v);
     }
 
