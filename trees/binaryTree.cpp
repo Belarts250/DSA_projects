79ec012ee923583c58c1151c70c2d8192cc770ea
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-22 00:11:27 +0200

    trees notes explained

---
+++ trees/binaryTree.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class BTNode{
+    public:
+    T data;
+    BTNode* left;
+    BTNode* right;
+
+    BTNode(T data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+
+    ~BTNode(){
+        delete left;
+        delete right;
+    }
+}
