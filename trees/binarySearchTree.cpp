f153b9a0c1d2a61c7389aee8df52d97a9ed2c443
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-22 00:23:35 +0200

    binary search trees

---
+++ trees/binarySearchTree.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class BST{
+    public:
+    int data;
+    BST* left;
+    BST* right;
+
+    BST(int data){
+        data = data;
+        
+    }
+}
