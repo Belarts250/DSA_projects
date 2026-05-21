9288ae59da17a78daa99377457e821a7424e61a2
Igihozo Belise <igihozobelise6@gmail.com>
2026-05-22 00:44:21 +0200

    binary search trees

--- trees/binarySearchTree.cpp
+++ trees/binarySearchTree.cpp
@@ -107,3 +107,16 @@ BST* delete(BST* root, int key){
 
     return root;
 }
+
+void inorder(Node* root) {
+
+    if(root == NULL) {
+        return;
+    }
+
+    inorder(root->left);
+
+    cout << root->data << " ";
+
+    inorder(root->right);
+}
